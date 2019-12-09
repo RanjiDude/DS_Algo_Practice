75f885763d99cc3ebe6413d837689cc124da54dd
Ranjith <ranjitation@gmail.com>
2019-12-08 21:06:36 -0600

    Add files via upload

--- astar.cpp
+++ astar.cpp
@@ -4,6 +4,16 @@
 #include <queue>
 #include <limits>
 #include <algorithm>
+#include "math.h"
+
+class compare
+{
+    public:
+        bool operator()(const std::vector<int> &a, const std::vector<int> &b)
+        {
+            return a[0] > b[0];
+        }
+};
 
 void print(std::vector<std::vector<int>> &graph)
 {
@@ -18,16 +28,36 @@ void print(std::vector<std::vector<int>> &graph)
     std::cout << std::endl;
 }
 
-std::vector<std::vector<int>> getneighbors(const std::vector<std::vector<int>> &graph, std::vector<int> node)
+void printpq(std::priority_queue<std::vector<int>, std::vector<std::vector<int>>, compare> q)
+{
+    while(!q.empty())
+    {
+        std::vector<int> temp = q.top();
+        std::cout << "[" << temp[0] << "," << temp[1] << "," << temp[2] << "] ";
+        q.pop();
+    }
+    std::cout << std::endl;
+}
+
+void printvector(const std::vector<int> &v)
+{
+    for (auto i:v)
+    {
+        std::cout << i << " ";
+    }
+    // std::cout << std::endl;
+}
+
+std::vector<std::vector<int>> getNeighbors(const std::vector<std::vector<int>> &graph, const std::vector<int> &node)
 {
     std::vector<std::vector<int>> neighbors;
-    std::vector<std::vector<int>> directions = {{0, 1}, {1, 1}, {1, 0}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
+    std::vector<std::vector<int>> directions = {{0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}, {1, 0}, {1, 1}};
     int x = 0, y = 0;
     for (int i=0; i<directions.size(); ++i)
     {
         x = node[0] + directions[i][0];
         y = node[1] + directions[i][1];
-        if (0 <= x and x < graph.size() and 0 <= y and y < graph[0].size() and graph[x][y] != -1)
+        if (0 <= x and x < graph.size() and 0 <= y and y < graph[0].size())
         {
             neighbors.push_back({x, y});
         }
@@ -35,7 +65,7 @@ std::vector<std::vector<int>> getneighbors(const std::vector<std::vector<int>> &
     return neighbors;
 }
 
-std::vector<std::vector<int>> getheuristic(std::vector<std::vector<int>> graph, const std::vector<int> &goal, char distance)
+std::vector<std::vector<int>> getHeuristic(std::vector<std::vector<int>> graph, const std::vector<int> &goal, char distance)
 {
     std::vector<std::vector<int>> h;
     for (int i=0; i<graph.size(); ++i)
@@ -47,29 +77,114 @@ std::vector<std::vector<int>> getheuristic(std::vector<std::vector<int>> graph, 
         }
     }
 
-    if (distance=='c')
+    std::queue<std::vector<int>> q;
+    q.push({0, goal[0], goal[1]});
+    while (!q.empty())
     {
-        std::queue<std::vector<int>> q;
-        q.push({0, goal[0], goal[1]});
-        while (!q.empty())
+        std::vector<int> node = q.front();
+        q.pop();
+        graph[node[1]][node[2]] = -1;
+        h[node[1]][node[2]] = node[0];
+        std::vector<std::vector<int>> nebs = getNeighbors(graph, {node[1], node[2]});
+        for (auto n : nebs)
         {
-            std::vector<int> node = q.front();
-            q.pop();
-            graph[node[1]][node[2]] = -1;
-            h[node[1]][node[2]] = node[0];
-            std::vector<std::vector<int>> nebs = getneighbors(graph, {node[1], node[2]});
-            for (auto n : nebs)
+            int heur;
+            if (distance == 'c')
             {
-                int heur = std::max(std::abs(goal[0]-n[0]), std::abs(goal[1]-n[1]));
+                heur = std::max(std::abs(goal[0]-n[0]), std::abs(goal[1]-n[1]));
+            }
+            else if (distance == 'm')
+            {
+                heur = std::abs(goal[0]-n[0]) + std::abs(goal[1]-n[1]);
+            }
+            // else if (distance == 'e')
+            // {
+            //     heur = std::pow(std::pow(goal[0]-n[0], 2) + std::pow(goal[1]-n[1], 2), 0.5);
+            // }
+            // q.push({heur, n[0], n[1]});
+
+            if (graph[n[0]][n[1]] != -1)
+            {
                 q.push({heur, n[0], n[1]});
             }
         }
-
     }
-            
+
     return h;
 }
 
+
+
+std::map<std::vector<int>, std::vector<int>> astar(std::vector<std::vector<int>> graph, const std::vector<int> &start, const std::vector<int> &goal, char distance)
+{
+    std::vector<std::vector<int>> h = getHeuristic(graph, goal, distance);
+    print(h);
+    std::map<std::vector<int>, std::vector<int>> m;
+    m.insert(std::pair<std::vector<int>, std::vector<int>>(start, {}));
+    std::priority_queue<std::vector<int>, std::vector<std::vector<int>>, compare> q;
+    q.push({0 + h[start[0]][start[1]], start[0], start[1]});
+    int s=0;
+    while(!q.empty())
+    {
+        // std::cout << "Iteration " << s++ << std::endl;
+        // print(graph);
+        // printpq(q);
+        std::vector<int> node = q.top();
+        // std::cout << "Chosen Node: " << "[" << node[1] << "," << node[2] << "] " << std::endl;
+        q.pop();
+        graph[node[1]][node[2]] = node[0];
+        if (node[1] == goal[0] and node[2] == goal[1])
+        {
+            break;
+        }
+        std::vector<std::vector<int>> nebs = getNeighbors(graph, {node[1], node[2]});
+        for (auto n: nebs)
+        {
+            if (graph[n[0]][n[1]] == 0)
+            {
+                m.insert(std::pair<std::vector<int>, std::vector<int>>(n, {node[1], node[2]}));
+                
+                int g = 0;
+                if (distance == 'c')
+                {
+                    g = std::max(std::abs(start[0]-n[0]), std::abs(start[1]-n[1]));
+                }
+                else if (distance == 'm')
+                {
+                    g = std::abs(start[0]-n[0]) + std::abs(start[1]-n[1]);
+                }
+                int f = 0;
+                f = g + h[n[0]][n[1]];
+                graph[n[0]][n[1]] = f;
+                // std::cout << "[" << f << "," << n[0] << "," << n[1] << "] " << std::endl;
+                q.push({f, n[0], n[1]});
+            }
+        }
+    }
+    print(graph);
+    return m;
+}
+
+void getPath(std::map<std::vector<int>, std::vector<int>> m,const std::vector<int> &goal)
+{
+    std::vector<std::vector<int>> path;
+    std::vector<int> current = goal;
+    while(!current.empty())
+    {
+        path.insert(path.begin(), current);
+        current = m[current];
+    }
+    for (int i=0; i<path.size(); ++i)
+    {
+        std::cout << "[" << path[i][0] << "," << path[i][1] << "]";
+        if (i != path.size()-1)
+        {
+            std::cout << "->";
+        }
+    }
+    std::cout << std::endl;
+    
+}
 
 
 int main()
@@ -81,7 +196,18 @@ int main()
                                            {0, 0, 0, 0, 0}};
 
     print(graph);
-    std::vector<std::vector<int>> h = getheuristic(graph, {0, 4}, 'c');
-    print(h);
+    std::vector<int> start = {0, 0}, goal = {4, 4};
+    // std::vector<std::vector<int>> h = getHeuristic(graph, goal, 'c');
+    // print(h);
+    std::map<std::vector<int>, std::vector<int>> pathnodes = astar(graph, start, goal, 'c');
+    // for (auto it=pathnodes.begin(); it!=pathnodes.end(); ++it)
+    // {
+    //     printvector(it->first);
+    //     std::cout << "=> ";
+    //     printvector(it->second);
+    //     std::cout << std::endl;
+    // }
+    getPath(pathnodes, goal);
+
 
 }
